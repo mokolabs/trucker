8b80617bf46c9c9ac22b6fb7bebf7329e5c44c8f
Giles Bowkett <gilesb@gmail.com>
2011-06-13 11:07:34 -0700

    switch to ivar: fucking crucial (see full comment)
    this allows you to reference the @new_record from elsewhere

--- lib/generators/templates/legacy_base.rb
+++ lib/generators/templates/legacy_base.rb
@@ -3,16 +3,16 @@ class LegacyBase < ActiveRecord::Base
   establish_connection "legacy"
   
   def migrate
-    new_record = self.class.to_s.gsub(/Legacy/,'::').constantize.new(map)
-    new_record[:id] = self.id
+    @new_record = self.class.to_s.gsub(/Legacy/,'::').constantize.new(map)
+    @new_record[:id] = self.id
     begin
-      new_record.save!
+      @new_record.save!
     rescue Exception => e
       # this is mostly for ActiveRecord Validation errors - if the validation fails, it
       # typically means you need to adjust the validations or the model you're migrating
       # your legacy data into. this is especially useful information when you're migrating
       # user data established with one auth library to a code base which uses another.
-      puts "error saving #{new_record.class} #{new_record.id}!"
+      puts "error saving #{@new_record.class} #{@new_record.id}!"
       puts e.inspect
     end 
   end 
